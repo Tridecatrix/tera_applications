e8415c788c6a6866f618f956305cf7bcf4b78cc7
Adnan Hasnat <u7300623@anu.edu.au>
2025-08-29 20:02:29 +1000

    updated config.sh

--- spark/config.sh
+++ spark/config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-TERAHEAP_REPO="/opt/carvguest/asplos23_ae/teraheap"
+# TERAHEAP_REPO="/opt/carvguest/asplos23_ae/teraheap"
+TERAHEAP_REPO="/home/users/u7300623/teraheap"
 
 # Export Allocator
 export LIBRARY_PATH=${TERAHEAP_REPO}/allocator/lib:$LIBRARY_PATH
@@ -10,9 +11,9 @@ export C_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include/:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include/:$CPLUS_INCLUDE_PATH
 
 # Set JAVA_HOME to use TeraHeap JVM
-export JAVA_HOME="${TERAHEAP_REPO}/jdk8u345/build/linux-x86_64-normal-server-release/jdk"
+export JAVA_HOME="${TERAHEAP_REPO}/jdk17u067/build/linux-x86_64-server-release/images/jdk"
 # Set up the path of TeraHeap applications
-TERA_APPS_REPO="/opt/carvguest/asplos23_ae/tera_applications"
+TERA_APPS_REPO="/home/users/u7300623/tera_applications"
 SPARK_VERSION="spark-3.3.0"
 #
 ########################################
