686a8591fb29ff834323f0a455c6e4b54be5148b
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-03 12:45:28 +1000

    Changed clear so it avoids clearing the inside of SparkBench

--- spark/scripts/clear.sh
+++ spark/scripts/clear.sh
@@ -9,7 +9,11 @@ CONF=$1
 . ./$CONF
 
 # Clear H2 and Shuffle
-find "$MNT_H2" -mindepth 1 ! -name 'SparkBench' ! -name 'lost+found' -exec rm -rf {} +
-find "$MNT_SHFL" -mindepth 1 ! -name 'SparkBench' ! -name 'lost+found' -exec rm -rf {} +
+find "$MNT_H2" -mindepth 1 \
+    \( -name 'SparkBench' -o -name 'lost+found' \) -prune \
+    -o -exec rm -r {} +
+find "$MNT_SHFL" -mindepth 1 \
+    \( -name 'SparkBench' -o -name 'lost+found' \) -prune \
+    -o -exec rm -r {} +
 
 ./clear-proc.sh
