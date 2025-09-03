c3edafd3b7cbdb3a0d46faac86f40b4a53761cbd
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-03 21:15:22 +1000

    refined clear scripts

--- spark/scripts/clear-proc.sh
+++ spark/scripts/clear-proc.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Kill processes
-echo "Ignore 2 no-such-process messages (from trying to kill grep and jps)"
+echo "Ignore 2 no-such-process messages (from ps | grep and jps including themselves in their outputs)"
+
+# kill run scripts
+pkill top_level_run.sh -f
+pkill run.sh
+
+./run.sh -k # kill background processes
 ps -u u7300623 -f | grep spark | awk '{print $2}' | xargs kill # kill all processes associated to spark (this includes scripts in spark directory)
 jps | awk '{print $1}' | xargs kill # kill java processes
 pkill iostat
--- spark/scripts/clear.sh
+++ spark/scripts/clear.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "usage: ./clear.sh [conf]"
-    exit
-fi
+usage() {
+    echo
+    echo "Usage:"
+    echo "      $0 [options ...] [-c conf.sh]"
+    echo "Options:"
+    echo "      -c  Path to conf.sh file"
+    echo "      -h  Show usage"
+    echo
+    exit 1
+}
 
-CONF=$1
-. ./$CONF
+while getopts ":c:h" opt
+do
+    case "${opt}" in
+        c)
+            CONF_FILE=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+. "${CONF_FILE}"
 
 # Clear H2 and Shuffle
-find "$MNT_H2" -mindepth 1 \
+find "$MNT_H2" -mindepth 1 -maxdepth 1 \
     \( -name 'SparkBench' -o -name 'lost+found' \) -prune \
     -o -exec rm -r {} +
-find "$MNT_SHFL" -mindepth 1 \
+find "$MNT_SHFL" -mindepth 1 -maxdepth 1 \
     \( -name 'SparkBench' -o -name 'lost+found' \) -prune \
     -o -exec rm -r {} +
 
