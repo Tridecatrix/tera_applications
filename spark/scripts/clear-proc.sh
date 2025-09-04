dc9b13dd1d43e37aa9be1a1a0f45ccb2408e460c
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-04 11:30:16 +1000

    fixed killing of run.sh scripts

--- spark/scripts/clear-proc.sh
+++ spark/scripts/clear-proc.sh
@@ -3,9 +3,9 @@
 echo "Ignore 2 no-such-process messages (from ps | grep and jps including themselves in their outputs)"
 
 # kill run scripts
-pkill top_level_run.sh -f
-pkill run.sh
+ps -u u7300623 -f | grep run.sh | awk '{print $2}' | xargs kill
 
+# kill all other processes
 ./run.sh -k # kill background processes
 ps -u u7300623 -f | grep spark | awk '{print $2}' | xargs kill # kill all processes associated to spark (this includes scripts in spark directory)
 jps | awk '{print $1}' | xargs kill # kill java processes
