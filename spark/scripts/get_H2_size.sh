26a453cf70b0ee051ecb9da551f6992eaabe9f61
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-04 20:30:48 +1000

    updated get H2 size script

--- spark/scripts/get_H2_size.sh
+++ spark/scripts/get_H2_size.sh
@@ -1,16 +1,39 @@
-BENCHMARKS=(
-    "ConnectedComponent"
-    "LinearRegression"
-    "LogisticRegression"
-    "PageRank"
-    "ShortestPaths"
-    # "SVDPlusPlus"
-    # "SVM"
-    # "TriangleCount"
-)
+#!/bin/bash
 
-for b in "${BENCHMARKS[@]}"; do
-    FILE="_2025-09-03-time-13-41-54/$b/run0/conf0/teraHeap.txt"
+usage() {
+    echo
+    echo "Usage:"
+    echo "      $0 -r rdir"
+    echo "Options:"
+    echo "      -r  Path to results directory"
+    echo "      -h  Show usage"
+    echo
+    exit 1
+}
+
+while getopts ":r:h" opt
+do
+    case "${opt}" in
+        r)
+            RDIR=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [[ -z $RDIR ]]; then
+    usage
+    exit
+fi
+
+for FILE in $(find "$RDIR" -name teraHeap.txt); do
+    RUNNAME=$(dirname $FILE)
     VALUE=$(grep "TOTAL_OBJECTS_SIZE" "$FILE" | awk 'max<$5{max=$5} END{print max}')
-    echo "$b: $(echo "scale=2; $VALUE / 1024.0 / 1024.0 / 1024.0" | bc -l) GB"
+    # NOTE: TOTAL_OBJECTS_SIZE IS ACTUALLY IN WORDS WHICH (as far as I can tell) MEANS 8 BYTES
+    echo "$RUNNAME: $(echo "scale=2; $VALUE * 8 / 1024.0 / 1024.0 / 1024.0" | bc -l) GB"
 done
