3cf444f3b5bf0eb009dc356de6435d2e9bf945f3
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-18 20:51:07 +1000

    update clear scripts

--- spark/scripts/clear-proc.sh
+++ spark/scripts/clear-proc.sh
@@ -5,7 +5,17 @@
 # Useful for cleaning up if a Spark run is interrupted.
 
 # kill run scripts
-ps -u u7300623 -f | grep run.sh | awk '{print $2, $8}' | while read pid cmd; do
+ps -u u7300623 -f | grep run.sh | awk '!/grep/ { \
+    pid=$2; \
+    if ($8 == "bash" || $8 ~ /\/bash$/) { \
+        script_name = $9; \
+        cmd = "bash " script_name; \
+    } else { \
+        cmd=""; \
+        for(i=8; i<=NF; i++) cmd=cmd $i " "; \
+    } \
+    print pid, cmd \
+}' | while read pid cmd; do
     if [[ $cmd == "grep" ]]; then
         continue; 
     fi
