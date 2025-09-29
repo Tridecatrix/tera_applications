b48dceb1e7e564130dd63d2d993f1afb7fd3e679
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-29 14:52:21 +1000

    updated zram usage script

--- spark/scripts/zram_usage.sh
+++ spark/scripts/zram_usage.sh
@@ -12,9 +12,9 @@
 ###################################################
 
 echoheader() {
-    echo "iostat  failed_reads failed_writes invalid_io notify_free"
-    echo "mmstat  orig_data_size compr_data_size mem_used_total mem_limit mem_used_max same_pages pages_compacted huge_pages huge_pages_since"
-    echo "bdstat  bd_count bd_reads bd_writes"
+    echo "ioheaders  failed_reads failed_writes invalid_io notify_free"
+    echo "mmheaders  orig_data_size compr_data_size mem_used_total mem_limit mem_used_max same_pages pages_compacted huge_pages huge_pages_since"
+    echo "bdheaders  bd_count bd_reads bd_writes"
     echo
 }
 
@@ -32,7 +32,7 @@ fi
 numOfExecutors=0
 
 # Clear existing statistics
-echo 0 | sudo tee /sys/fs/${ZRAMDEV}/mem_used_max > /dev/null
+echo 0 | sudo tee /sys/block/${ZRAMDEV}/mem_used_max > /dev/null
 
 # Wait here until the executors are launched
 while [ ${numOfExecutors} -lt "${NUM_OF_EXECUTORS}" ] 
@@ -42,13 +42,13 @@ do
 done
 
 # Echo header line (for human readability)
-echoheader
+echoheader >> "${OUTPUT}"
 while true; do
     # zramctl --output-all >> "${OUTPUT}"
     echo "iostat  $(cat /sys/block/${ZRAMDEV}/io_stat)" >> "${OUTPUT}"
     echo "mmstat  $(cat /sys/block/${ZRAMDEV}/mm_stat)" >> "${OUTPUT}"
     echo "bdstat  $(cat /sys/block/${ZRAMDEV}/bd_stat)" >> "${OUTPUT}"
-    echo
+    echo >> "${OUTPUT}"
     
     sleep 1
 done
