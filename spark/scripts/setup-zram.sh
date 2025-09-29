4daae569a4c4f60187bcce5237ab0b77c0a0eabf
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-29 14:47:30 +1000

    adjust comment in setup-zram

--- spark/scripts/setup-zram.sh
+++ spark/scripts/setup-zram.sh
@@ -6,7 +6,7 @@
 
 usage() {
   echo "Usage: ./setup-zram.sh [-s DEVICE_SIZE] [-m MEMORY_LIMIT] [-h]"
-  echo "Suffixes allowed for both of these values" 
+  echo "Both values are in bytes; suffixes allowed" 
 }
 
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
