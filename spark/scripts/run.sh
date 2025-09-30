a92d3d49e2f381a8e413a39b15bcb79f504e2ee1
Adnan Hasnat <u7300623@anu.edu.au>
2025-09-30 20:28:04 +1000

    added parsing teraheap stats to the run script

--- spark/scripts/run.sh
+++ spark/scripts/run.sh
@@ -525,6 +525,8 @@ do
         TH_METRICS=$(ls -td "${SPARK_DIR}"/work/* | head -n 1)
         cp "${TH_METRICS}"/0/teraHeap.txt "${RUN_DIR}"/
         ./parse_results.sh -d "${RUN_DIR}" -n "${NUM_EXECUTORS}" -t
+
+        ./parse_teraheap_stats.sh "${RUN_DIR}/teraHeap.txt"
       else
         ./parse_results.sh -d "${RUN_DIR}" -n "${NUM_EXECUTORS}" -s
       fi
